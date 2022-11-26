e5397d668cfa480ecdffbe5c96152720e39887a5
ilham B <durianbohong@gmail.com>
2022-11-26 20:21:31 +0700

    modified:   dom.js

--- dom.js
+++ dom.js
@@ -1,21 +1,33 @@
-( () => {
+/**
+ * Copyright 2021 Ilham B
+ * Modifications copyright (C) 2022 Ilham B
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-    console.log("HALLO NERD :)")
-
-} )();
-
-var DOM = (
-    element = "div", 
+ var DOM = (
+    element = "div",
     data = {
-        attr: {}, 
-        inner: [], 
-        todo: null}
-        ) => {
+        attr: {},
+        inner: [],
+        todo: null
+    }
+) => {
 
     let a = document.createElement(element),
-    attr = data ? data.attr : {},
-    inner = data ? data.inner : '',
-    todo = data ? data.todo : null;
+        attr = data ? data.attr : {},
+        inner = data ? data.inner : '',
+        todo = data ? data.todo : null;
 
     if (attr) {
         Object.keys(attr).forEach((b) => {
@@ -30,13 +42,19 @@ var DOM = (
      */
     a.setInner = (c) => {
         switch (typeof c) {
+            case 'function':
+                a.innerHTML = null
+                a.append(c(a))
+                break;
+
             case 'boolean':
-            case 'string' :
+            case 'string':
                 a.innerHTML = c;
                 break;
+
             case 'object':
-                if(Array.isArray(c)){
-                    c.forEach((d)=>{
+                if (Array.isArray(c)) {
+                    c.forEach((d) => {
                         a.append(d);
                     });
                 } else {
@@ -64,7 +82,7 @@ var DOM = (
      * @param {int} maxLength - Minimum karakter
      * @param {regex} exeption - Karakter ilegal
      */
-    a.onInput = function(minLength, maxLength, exeption = "//g") {
+    a.onInput = function (minLength, maxLength, exeption = "//g") {
         a.addEventListener("keypress", event => {
             if (event.keyCode != 8 && event.key.match(exeption)) {
                 event.preventDefault();
@@ -89,9 +107,11 @@ var DOM = (
             a.setAttribute("minlength", minLength);
         });
     };
-    
+
     if (inner) a.setInner(inner);
     if (todo) todo(a);
-    
+
     return a;
 }
+
+export {DOM};
